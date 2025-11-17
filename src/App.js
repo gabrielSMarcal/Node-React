c20a2b251199a463af172871195c7b47081fad95
gabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-11-17 19:06:04 -0300

    Fix link text in App component to use English "Learn React"

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende React
+          Learn React
         </a>
       </header>
     </div>
