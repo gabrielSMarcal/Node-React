7ac8b07e8384592f1341dc2957d13e7b052624e3
GabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-11-19 18:44:27 -0300

    feat: create Titulo component for consistent styling and integrate it into UltimosLancamentos

--- src/components/UltimosLancamentos/index.js
+++ src/components/UltimosLancamentos/index.js
@@ -1,22 +1,13 @@
 import styled from 'styled-components'
 
-import {livros} from './dadosUltimosLancamentos'
+import { livros } from './dadosUltimosLancamentos'
+import { Titulo } from '../Titulo'
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -30,7 +21,10 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-            <Titulo>ÚLTIMOS LANÇAMENTOS</Titulo>
+            <Titulo
+                cor='#EB9B00'
+                tamanhoFonte='36px'
+            >ÚLTIMOS LANÇAMENTOS</Titulo>
             <NovosLivrosContainer>
                 {livros.map(livro => (
                     <img src={livro.src} alt={livro.nome}/>
